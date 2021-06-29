0caad881604868792ed636710596134240147c73
아르 <parkjm514@slogup.com>
2021-06-29 17:15:55 +0900

    change about auth-nums and first commit tokens

--- apis/account/auth-num-tokens/post.js
+++ apis/account/auth-num-tokens/post.js
@@ -1,10 +1,22 @@
 const utils = require('../../../utils')
 const std = require('../../../standards')
+const authNumStd = std.authNum
 
 module.exports = {
     validation: () => {
         return (req, res, next) => {
-            
+            let body = req.body
+            let authNum = body.authNum
+
+            if (body.type === undefined || body.key === undefined || authNum === undefined){
+                return res.status(400).json({code: "400_1"})
+            }
+            if (body.type !== authNumStd.authNumTypePhone && body.type !== authNumStd.authNumTypeEmail){
+                return res.status(400).json({code: "400_2"})
+            }
+            if (typeof authNum !== "string" || authNum.length !== authNumStd.length){
+                return res.status(400).json({code: "400_5"})
+            }
 
             next()
         }
@@ -12,13 +24,47 @@ module.exports = {
 
     encryption: () => {
         return (req, res, next) => {
+            req.encryptNum = utils.encryption(req.body.authNum)
+
             next()
         }
     },
 
     validationAutNum: (db) => {
         return (req, res, next) => {
-            
+            let schema = db.schema.auth
+            let AuthNum = req.encryptNum
+            let body = req.body
+            let minute = authNumStd.expiredMinute
+            let now = Date.now()
+            let bAuthNum = false
+
+            //todo: db안에 인증번호가 없을때와 있긴한데 틀릴때를 구분해줘야할까
+
+            for (let authPk in schema){
+                if(schema[authPk].type === body.type && schema[authPk].key === body.key){
+                    if ([now - schema[authPk].createdAt] / 60000 > minute){
+                        return res.status(400).json({code: "400_6"})
+                    }
+                    if(schema[authPk].encryptNum === undefined){
+                        return res.status(404).json({code: "404_1"})
+                    }
+                    if (schema[authPk].encryptNum !== AuthNum){
+                        return res.status(401).json({code: "401_1"})
+                    }else{
+                        bAuthNum = true
+                        break
+                    }
+                }
+            }
+
+            if (bAuthNum === false){
+                if (body.type === authNumStd.authNumTypePhone){
+                    return res.status(400).json({code: "400_4"})
+                }else if (body.type === authNumStd.authNumTypeEmail){
+                    return res.status(400).json({code: "400_3"})
+                }
+            }
 
             next()
         }
@@ -26,13 +72,41 @@ module.exports = {
 
     tokenGenerator: () => {
         return (req, res, next) => {
+            req.tokenNum = utils.createRandomString(authNumStd.tokenLength)
+
             next()
         }
     },
 
     syncDB: (db) => {
         return (req, res, next) => {
-          
+            let body = req.body
+            let tokenNum = req.tokenNum
+            let tokenSchema = db.schema.authToken
+            let tokenPk = db.pk.authTokenPk
+            let createdAt = Date.now()
+            let bToken = false
+
+            for (let pk in tokenSchema){
+                if(body.type === tokenSchema[pk].type && body.key === tokenSchema[pk].key){
+                    tokenSchema[pk].tokenNum = tokenNum
+                    tokenSchema[pk].createdAt = createdAt
+                    bToken = true
+                    break
+                }
+            }
+
+            if(bToken === false){
+                tokenSchema[tokenPk] = {
+                    type: body.type,
+                    key: body.key,
+                    tokenNum: tokenNum,
+                    createdAt: createdAt,
+                    tokenExpiredMinute: authNumStd.tokenExpiredMinute
+                }
+                tokenPk++
+            }
+
 
             next()
         }
--- apis/account/auth-nums/post.js
+++ apis/account/auth-nums/post.js
@@ -7,34 +7,29 @@ module.exports = {
         return (req, res, next) => {
             let body = req.body
             let emailRule = /^(01[016789]{1}|02|0[3-9]{1}[0-9]{1})-?[0-9]{3,4}-?[0-9]{4}$/
-            let phoneRule = /^(01[016789]{1}|02|0[3-9]{1}[0-9]{1})-?[0-9]{3,4}-?[0-9]{4}$/
+            let phoneRule = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i
 
-            if(body.type !== authNumStd.authNumTypeEmail && body.type !== authNumStd.authNumTypePhone){
-                res.status(400)
-                return res.json({code: "400_1"})
+            if (body.type !== authNumStd.authNumTypeEmail && body.type !== authNumStd.authNumTypePhone) {
+                return res.status(400).json({code: "400_1"})
             }
-//필수값,데이터형태,데이터길이 체크
+            //todo:필수값,데이터형태,데이터길이 체크, todo이용하기, is eamil, is는 함수 b붙이기
 
-            if(body.type === undefined || body.key === undefined){
+            if (body.type === undefined || body.key === undefined) {
                 res.status(400)
                 return res.json({code: "400_2"})
             }
 
-            if(body.type === authNumStd.authNumTypeEmail){
-                if(emailRule.test(body.key) !== true){
+            if (body.type === authNumStd.authNumTypeEmail) {
+                if (emailRule.test(body.key) !== true) {
                     res.status(400)
-                   return res.json({code: "400_3"})
+                    return res.json({code: "400_3"})
                 }
-            }
-
-            else if(body.type === authNumStd.authNumTypePhone){
-                if(phoneRule.test(body.key) !== true){
+            } else if (body.type === authNumStd.authNumTypePhone) {
+                if (phoneRule.test(body.key) !== true) {
                     res.status(400)
                     return res.json({code: "400_4"})
                 }
-            }
-
-            else{
+            } else {
                 return console.log("error validation")
             }
 
@@ -64,27 +59,30 @@ module.exports = {
             let encryptNum = req.encryptNum
             let authSchema = db.schema.auth
             let authPk = db.pk.authPk
-            let checking = false
-            //길이 유효성검사
-            // if(encryptNum.length !== std.authNum.length){
-            //     res.status(400).json({code: "400_5"})
-            // }
+            let bAuth = false
+            let createdAt = Date.now()
+                //TODO:길이 유효성검사
+                // TODO:if(encryptNum.length !== std.authNum.length){
+                //TODO:     res.status(400).json({code: "400_5"})
+                // }
+                // 시간은 무조건 서버가하기,클라가 하면은 맘대로 조작될 가능성이 있음, 만약에 그렇지 않다면 유효성 검사, []안에 변수 가능, type도 호출
+                // new date 보다 date now 사용
 
-            for(let path in authSchema){
-                if(authSchema[path].key === body.key){
-                    authSchema[path].createdAt = body.createdAt
+            for (let path in authSchema) {
+                if (authSchema[path].key === body.key) {
+                    authSchema[path].type = body.type
+                    authSchema[path].createdAt = createdAt
                     authSchema[path].encryptNum = encryptNum
-                    checking = true
+                    bAuth = true
                     break
                 }
             }
-//1:[type ,key, createdat]
 
-            if(checking === false) {
+            if (bAuth === false) {
                 authSchema[authPk] = {
                     type: body.type,
                     key: body.key,
-                    createdAt: body.createdAt,
+                    createdAt: createdAt,
                     authNum: encryptNum
                 }
                 authPk++
