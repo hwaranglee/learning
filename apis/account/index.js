9e8362bfa4f7e81d06a219ed0edc629767c0a4ea
심종훈 <jhshim@slogup.com>
2021-07-05 14:51:23 +0900

    done: prettier 세팅

--- apis/account/index.js
+++ apis/account/index.js
@@ -1,45 +1,57 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const db = require('../../db/index')
+const db = require("../../db/index");
 
-const authNumsPost = require('./auth-nums/post')
-const authNumTokensPost = require('./auth-num-tokens/post')
-const usersPost = require('./users/post')
+const authNumsPost = require("./auth-nums/post");
+const authNumTokensPost = require("./auth-num-tokens/post");
+const usersPost = require("./users/post");
 // validator 연습용
-const validatorPost = require('./validator/post')
+const validatorPost = require("./validator/post");
 
 module.exports = router;
 
-router.post('/auth-nums',
-    authNumsPost.validation(),
-    authNumsPost.authNumGenerator(),
-    authNumsPost.encryption(),
-    authNumsPost.syncDB(db),
-    authNumsPost.responder()
-)
+router.post(
+  "/auth-nums",
+  authNumsPost.validation(),
+  authNumsPost.authNumGenerator(),
+  authNumsPost.encryption(),
+  authNumsPost.syncDB(db),
+  authNumsPost.responder()
+);
 
-router.post('/auth-num-tokens',
-    authNumTokensPost.validation(),
-    authNumTokensPost.encryption(),
-    authNumTokensPost.validationAutNum(db),
-    authNumTokensPost.tokenGenerator(),
-    // JWT를 사용하였기 때문에 db에 token을 저장하지 않는다.
-    // authNumTokensPost.syncDB(db),
-    authNumTokensPost.responder()
-)
+router.post(
+  "/auth-num-tokens",
+  authNumTokensPost.validation(),
+  authNumTokensPost.encryption(),
+  authNumTokensPost.validationAutNum(db),
+  authNumTokensPost.tokenGenerator(),
+  // JWT를 사용하였기 때문에 db에 token을 저장하지 않는다.
+  // authNumTokensPost.syncDB(db),
+  authNumTokensPost.responder()
+);
 
-router.post('/users',
-    usersPost.validation(),
-    usersPost.tokenVerifier(),
-    usersPost.passwordEncryption(),
-    usersPost.syncDBUser(db),
-    usersPost.syncDBOptionalTerms(db),
-    usersPost.responder()
-)
+router.post(
+  "/users",
+  usersPost.validation(),
+  usersPost.tokenVerifier(),
+  usersPost.passwordEncryption(),
+  usersPost.syncDBUser(db),
+  usersPost.syncDBOptionalTerms(db),
+  usersPost.responder()
+);
 
 // validator 연습용
-router.post('/validator',
-    validatorPost.validation(),
-    validatorPost.responder()
-)
+router.post(
+  "/validator",
+  validatorPost.validation(),
+  validatorPost.responder()
+);
+
+// 테스트용
+router.post("/test", (req, res, next) => {
+  const ip = req.header["x-forwarded-for"] || req.connection.remoteAddress;
+  console.log("ip: ", ip);
+  console.log("user-agent: ", req.headers["user-agent"]);
+  res.json("hello");
+});
--- apis/account/users/post.js
+++ apis/account/users/post.js
@@ -1,126 +1,131 @@
-const {verify} = require('jsonwebtoken')
+const { verify } = require("jsonwebtoken");
 // todo express-validator로 refactor
 // const {body, validationResult} = require('express-validator')
 
-const utils = require('../../../utils')
+const utils = require("../../../utils");
 
 module.exports = {
-    validation: () => {
-        return (req, res, next) => {
-            const {account, password} = req.body
-            const authorization = req.headers.authorization
+  validation: () => {
+    return (req, res, next) => {
+      const { account, password } = req.body;
+      const authorization = req.headers.authorization;
 
-            if (account === undefined || password === undefined || authorization === undefined) {
-                return res.status(400).json({code: "400_1"})
-            }
+      if (
+        account === undefined ||
+        password === undefined ||
+        authorization === undefined
+      ) {
+        return res.status(400).json({ code: "400_1" });
+      }
 
-            const regExpAccount = /^[0-9a-z]{6,12}$/
-            if (!regExpAccount.test(account) || typeof account !== "string") {
-                return res.status(400).json({code: "400_7"})
-            }
+      const regExpAccount = /^[0-9a-z]{6,12}$/;
+      if (!regExpAccount.test(account) || typeof account !== "string") {
+        return res.status(400).json({ code: "400_7" });
+      }
 
-            const regExpPassword = /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+])(?!.*[^a-zA-z0-9$`~!@$!%*#^?&\\(\\)\-_=+]).{8,16}$/
-            if (!regExpPassword.test(password)) {
-                return res.status(400).json({code: "400_8"})
-            }
+      const regExpPassword =
+        /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+])(?!.*[^a-zA-z0-9$`~!@$!%*#^?&\\(\\)\-_=+]).{8,16}$/;
+      if (!regExpPassword.test(password)) {
+        return res.status(400).json({ code: "400_8" });
+      }
 
-            next()
+      next();
+    };
+  },
+
+  tokenVerifier: () => {
+    return (req, res, next) => {
+      const authorization = req.headers.authorization;
+
+      try {
+        verify(authorization, process.env.SECRET_KEY);
+
+        next();
+      } catch (err) {
+        return res.status(400).json({ code: "400_9" });
+      }
+    };
+  },
+
+  passwordEncryption: () => {
+    return (req, res, next) => {
+      // salt에 account VS column 만들기
+      const { password } = req.body;
+      const encryption = utils.encryption;
+
+      req.salt = encryption(Date.now().toString());
+      req.password = encryption(password + encryption(req.salt));
+
+      next();
+    };
+  },
+
+  syncDBUser: (db) => {
+    return (req, res, next) => {
+      const { account } = req.body;
+      const { password, salt } = req;
+      const user = db.schema.user;
+      const pk = db.pk;
+
+      // account 중복 확인
+      for (let userPk in user) {
+        if (user[userPk].account === account) {
+          // ! (질문)예를 들어 아래와 같은 status code를 정하는 데 어느 정도의 시간을 투자하는 게 맞을까?
+          return res.status(409).json({ code: "409_1" });
         }
-    },
+      }
 
-    tokenVerifier: () => {
-        return (req, res, next) => {
-            const authorization = req.headers.authorization
+      // cf> 가입 도중 서버가 터졌다면?(transaction)
+      // user table에만 data가 저장된 상태에서, 다음 단계의 optionalTerms에는 data가 저장되지 못한 채 서버가 터졌다고 가정해보자.
+      // 이와 같은 상황에서 해당 유저가 다시 한번 회원가입 절차를 밟는다고 가정했을 때, 이전에 기입한 account는 사용할 수 없게 된다.
+      // 이미 db에 해당 account가 저장되었기 때문이다.
+      // 이와 같은 상황을 방지하려면 (1)현재의 코드상황에서 어떻게 하면 좋을 지, (2)실제 상황에서는 transaction을 어떻게 구현할 것인지 생각해보자.
 
-            try {
-                verify(authorization, process.env.SECRET_KEY)
+      user[pk.userPk] = {
+        account,
+        password,
+        salt,
+        createdAt: Date.now(),
+      };
 
-                next()
-            } catch (err) {
-                return res.status(400).json({code: "400_9"})
-            }
-        }
-    },
+      req.userPk = pk.userPk;
+      pk.userPk++;
 
-    passwordEncryption: () => {
-        return (req, res, next) => {
-            // salt에 account VS column 만들기
-            const {password} = req.body
-            const encryption = utils.encryption
+      // ! check
+      console.log("user: ", user);
 
-            req.salt = encryption(Date.now().toString())
-            req.password = encryption(password + encryption(req.salt))
+      next();
+    };
+  },
 
-            next()
-        }
-    },
+  syncDBOptionalTerms: (db) => {
+    return (req, res, next) => {
+      const { BTerms1, BTerms2, BTerms3 } = req.body;
+      // todo spread syntax/rest parameter 활용해서 좀 더 일반적인 코드로 변경 가능할까?
 
-    syncDBUser: (db) => {
-        return (req, res, next) => {
-            const {account} = req.body
-            const {password, salt} = req
-            const user = db.schema.user
-            const pk = db.pk
+      const optionalTerms = db.schema.optionalTerms;
+      const pk = db.pk;
 
-            // account 중복 확인
-            for (let userPk in user) {
-                if (user[userPk].account === account) {
-                    // ! (질문)예를 들어 아래와 같은 status code를 정하는 데 어느 정도의 시간을 투자하는 게 맞을까?
-                    return res.status(409).json({code: "409_1"})
-                }
-            }
+      optionalTerms[pk.optionalTermsPk] = {
+        userPk: req.userPk, // foreign key
+        BTerms1,
+        BTerms2,
+        BTerms3,
+      };
 
-            // cf> 가입 도중 서버가 터졌다면?(transaction)
-            // user table에만 data가 저장된 상태에서, 다음 단계의 optionalTerms에는 data가 저장되지 못한 채 서버가 터졌다고 가정해보자.
-            // 이와 같은 상황에서 해당 유저가 다시 한번 회원가입 절차를 밟는다고 가정했을 때, 이전에 기입한 account는 사용할 수 없게 된다.
-            // 이미 db에 해당 account가 저장되었기 때문이다.
-            // 이와 같은 상황을 방지하려면 (1)현재의 코드상황에서 어떻게 하면 좋을 지, (2)실제 상황에서는 transaction을 어떻게 구현할 것인지 생각해보자.
+      pk.optionalTermsPk++;
 
-            user[pk.userPk] = {
-                account,
-                password,
-                salt,
-                createdAt: Date.now()
-            }
+      // ! check
+      // console.log("optionalTerms: ", optionalTerms)
+      // console.log("pk: ", pk)
 
-            req.userPk = pk.userPk
-            pk.userPk++
+      next();
+    };
+  },
 
-            // ! check
-            console.log("user: ", user)
-
-            next()
-        }
-    },
-
-    syncDBOptionalTerms: (db) => {
-        return (req, res, next) => {
-            const {BTerms1, BTerms2, BTerms3} = req.body
-            // todo spread syntax/rest parameter 활용해서 좀 더 일반적인 코드로 변경 가능할까?
-
-            const optionalTerms = db.schema.optionalTerms
-            const pk = db.pk
-
-            optionalTerms[pk.optionalTermsPk] = {
-                userPk: req.userPk, // foreign key
-                BTerms1,
-                BTerms2,
-                BTerms3
-            }
-
-            pk.optionalTermsPk++
-
-            // ! check
-            // console.log("optionalTerms: ", optionalTerms)
-            // console.log("pk: ", pk)
-
-            next()
-        }
-    },
-
-    responder: () => {
-        return (req, res, next) => {
-            res.status(201).json({code: "201_1"})
-        }
-    }
-}
+  responder: () => {
+    return (req, res, next) => {
+      res.status(201).json({ code: "201_1" });
+    };
+  },
+};
